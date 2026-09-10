68681b4b7926216c96f9f6429a39b0dfc17ff4a1
Jingpo Phai <phaijingpo15102006@gmail.com>
2026-09-10 16:14:56 +0700

    Update db.js
    test

--- db/db.js
+++ db/db.js
@@ -2,10 +2,10 @@ require('dotenv').config(); // Load environment variables from a .env fil
 const { Connection } = require('mysql2');
 const mysql = require('mysql2/promise');
 const db = mysql.createPool({
-    host: process.env.DB_HOST||localhost,
-    user: process.env.DB_USER ||root,
-    password: process.env.DB_PASSWORD|| 151006,
-    database: process.env.DB_NAME|| user
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.getConnection()

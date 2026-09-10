b4e12760ab3e02ba6043e25351c85f306057e66d
Jingpo <128562692+Jingpo15102006@users.noreply.github.com>
2026-09-10 16:42:01 +0700

    first commit

--- Controller/product.controller.js
+++ Controller/product.controller.js
@@ -12,7 +12,7 @@ const addProduct = async (req, res) => {
 };
 
 const getProduct = async (req, res) => {
-<<<<<<< HEAD
+
 
     res.status(200).json({ message: "Get Product Successfully" });
     // res.status(2
@@ -24,18 +24,6 @@ const getProduct = async (req, res) => {
     // } catch (err) {
     //     res.status(500).json({ error: err.message });
 
-=======
-    res.status(200).json({ message: "Get Product Successfully" });
-    // try {
-    //     const sql = "select *from user.products";
-    //     const results = await db.execute(sql);
-    //     res.status(200).json(results);
-
-    // } catch (err) {
-    //     res.status(500).json({ error: err.message });
-
->>>>>>> 68681b4b7926216c96f9f6429a39b0dfc17ff4a1
-    // }
 };
 
 const deleteProduct = async (req, res) => {

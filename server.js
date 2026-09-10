4205c936cdf3f07617a7287380bb420967741414
Jingpo Phai <phaijingpo15102006@gmail.com>
2026-09-10 15:54:54 +0700

    Update server.js
    cc

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.use('/api', vieworder);
 
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
